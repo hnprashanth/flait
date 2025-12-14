ed6dfec79b6698c7ce400382bae92f0f0f795b02
Prashanth HN <prashanth@prashanth.net>
2025-12-14 18:52:06 +0530

    Initial commit

---
+++ test/flait.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Flait from '../lib/flait-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/flait-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Flait.FlaitStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
